1eb959dc625b83f6a4b93a5b48c332e19622e64f
rwilson1234 <wilson.kopite@gmail.com>
2024-11-07 16:27:22 +0000

    root.tsx > extra credit UI pills

--- app/root.tsx
+++ app/root.tsx
@@ -16,8 +16,12 @@ export default function App() {
 		<Document nonce={nonce}>
 			<div className="flex h-screen flex-col justify-between">
 				<div className="flex-1">
-					<main className="grid h-full place-items-center">
-						<h1 className="text-mega">Welcome to Epic News!</h1>
+					<main className="bg-red-300 grid h-full place-items-center">
+						<h1 className="text-mega">Welcome to the world of NEWS!</h1>
+						<p className="text-base text-gray-600 md:text-lg lg:text-2xl">Welcome to Epic News, where the latest developments in tech are found.</p>
+						<button class="bg-red-500 text-white py-2 px-10 rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 sm:px-6 md:px-8 lg:px-10 xl:px-12">
+  Click Me
+</button>
 					</main>
 				</div>
 			</div>
