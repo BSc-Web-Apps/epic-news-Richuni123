1743b41427753c1b99d93543676e9640aa918fad
rwilson1234 <wilson.kopite@gmail.com>
2024-11-14 14:04:25 +0000

    added 3 buttons to home page

--- app/root.tsx
+++ app/root.tsx
@@ -2,7 +2,6 @@ import { type LinksFunction } from '@remix-run/node'
 import Document from '~/components/shared-layout/Document'
 import { useNonce } from '~/utils/nonce-provider.ts'
 import rootLinkElements from '~/utils/providers/rootLinkElements'
-
 export const links: LinksFunction = () => {
 	return rootLinkElements
 }
@@ -16,12 +15,24 @@ export default function App() {
 		<Document nonce={nonce}>
 			<div className="flex h-screen flex-col justify-between">
 				<div className="flex-1">
-					<main className="bg-red-300 grid h-full place-items-center">
+					<main className="container bg-red-300 grid h-full place-items-center">
 						<h1 className="text-mega">Welcome to the world of NEWS!</h1>
 						<p className="text-base text-gray-600 md:text-lg lg:text-2xl">Welcome to Epic News, where the latest developments in tech are found.</p>
-						<button class="bg-red-500 text-white py-2 px-10 rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 sm:px-6 md:px-8 lg:px-10 xl:px-12">
+						
+						
+<div className="flex gap-8">
+						<button className="bg-red-500 text-white py-2 px-10 rounded hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-blue-400 sm:px-6 md:px-8 lg:px-10 xl:px-12">
   Click Me
 </button>
+
+<button className="bg-red-500 text-white py-2 px-10 rounded hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-blue-400 sm:px-6 md:px-8 lg:px-10 xl:px-12">
+  Click Me
+</button>
+<button className="bg-red-500 text-white py-2 px-10 rounded hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-blue-400 sm:px-6 md:px-8 lg:px-10 xl:px-12">
+  Click Me
+</button>
+</div>
+
 					</main>
 				</div>
 			</div>
