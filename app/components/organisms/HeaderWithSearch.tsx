7d1d777a749633331eefb00152d54d3ea174f554
rwilson1234 <wilson.kopite@gmail.com>
2024-11-14 16:29:20 +0000

    adding logo to header and footer

--- app/components/organisms/HeaderWithSearch.tsx
+++ app/components/organisms/HeaderWithSearch.tsx
@@ -1,4 +1,5 @@
 import { useMatches } from '@remix-run/react'
+import logo from '~/assets/svg/Vector.svg'
 import { SearchBar } from '../molecules/SearchBar'
 import LoginOrUserDropdown from './LoginOrUserDropdown'
 
@@ -8,8 +9,11 @@ export default function HeaderWithSearch() {
 	const searchBar = isOnSearchPage ? null : <SearchBar status="idle" />
 
 	return (
-		<header className="bg-primary/10 py-6 dark:bg-dark-primary/10">
+		<header className="dark:bg-dark-primary/10 bg-primary/10 py-6">
 			<nav className="container flex flex-wrap items-center justify-between gap-4 sm:flex-nowrap md:gap-8">
+				<div className="flex h-20 w-28 items-center justify-center rounded bg-red-600 p-6">
+					<img src={logo} alt=" News Logo" className="w-16" />
+				</div>
 				<div className="ml-auto hidden max-w-sm flex-1 sm:block">
 					{searchBar}
 				</div>
