7e3520f2090881a737edd9936a1afeae8b6d6f57
rwilson1234 <wilson.kopite@gmail.com>
2024-11-14 15:27:56 +0000

    implemented light and dark mode

--- app/root.tsx
+++ app/root.tsx
@@ -1,7 +1,11 @@
 import { type LinksFunction } from '@remix-run/node'
+import { useLoaderData } from '@remix-run/react'
 import Document from '~/components/shared-layout/Document'
+import ThemeSwitch from '~/components/shared-layout/ThemeSwitch'
 import { useNonce } from '~/utils/nonce-provider.ts'
 import rootLinkElements from '~/utils/providers/rootLinkElements'
+import { type loader } from './__root.server'
+import useTheme from './hooks/useTheme.tsx'
 export const links: LinksFunction = () => {
 	return rootLinkElements
 }
@@ -9,32 +13,37 @@ export { headers, meta } from './__root.client.tsx'
 export { action, loader } from './__root.server.tsx'
 
 export default function App() {
-	const nonce = useNonce()
+	const data = useLoaderData<typeof loader>();
+	const nonce = useNonce();
+	const theme = useTheme()
 
 	return (
-		<Document nonce={nonce}>
+		<Document nonce={nonce} theme={theme}>
 			<div className="flex h-screen flex-col justify-between">
 				<div className="flex-1">
-					<main className="container bg-red-300 grid h-full place-items-center">
+					<main className="container bg-white-300 grid h-full place-items-center">
 						<h1 className="text-mega">Welcome to the world of NEWS!</h1>
 						<p className="text-base text-gray-600 md:text-lg lg:text-2xl">Welcome to Epic News, where the latest developments in tech are found.</p>
 						
 						
-<div className="flex gap-8">
+			<div className="flex gap-8">
 						<button className="bg-red-500 text-white py-2 px-10 rounded hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-blue-400 sm:px-6 md:px-8 lg:px-10 xl:px-12">
-  Click Me
-</button>
+  							Click Me
+						</button>
 
-<button className="bg-red-500 text-white py-2 px-10 rounded hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-blue-400 sm:px-6 md:px-8 lg:px-10 xl:px-12">
-  Click Me
-</button>
-<button className="bg-red-500 text-white py-2 px-10 rounded hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-blue-400 sm:px-6 md:px-8 lg:px-10 xl:px-12">
-  Click Me
-</button>
-</div>
+						<button className="bg-red-500 text-white py-2 px-10 rounded hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-blue-400 sm:px-6 md:px-8 lg:px-10 xl:px-12">
+						Click Me
+						</button>
+						<button className="bg-red-500 text-white py-2 px-10 rounded hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-blue-400 sm:px-6 md:px-8 lg:px-10 xl:px-12">
+						Click Me
+						</button>
+					</div>
 
 					</main>
 				</div>
+				<div className="container flex justify-between pb-5">
+          <ThemeSwitch userPreference={data.requestInfo.userPrefs.theme} />
+        </div>
 			</div>
 		</Document>
 	)
