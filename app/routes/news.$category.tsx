1d39f2a06d3828c790718dde1b79d280f5fae69d
rwilson1234 <0554244@hc.ac.je>
2024-12-03 21:31:18 +0000

    load and display up to article card

--- app/routes/news.$category.tsx
+++ app/routes/news.$category.tsx
@@ -2,38 +2,40 @@ import { invariant } from '@epic-web/invariant'
 import { type LoaderFunctionArgs, json } from '@remix-run/node'
 import { useLoaderData } from '@remix-run/react'
 import { toTitleCase } from '~/utils/stringUtils.ts'
+import { prisma } from '~/utils/db.server.ts'
+
 export async function loader({ params }: LoaderFunctionArgs) {
 	const { category } = params
 	invariant(typeof category === 'string', 'Category not found')
 
 	const categoryTitle = toTitleCase(category)
+	const allArticles = await prisma.article.findMany({
+		select: {
+			id: true,
+			title: true,
+			category: { select: { name: true } },
+			images: { select: { id: true } },
+		},
+	})
 
-	return json({ categoryTitle })
-}
-const WireframeBlock = () => {
-	return (
-		<div className="h-72 animate-pulse rounded-lg bg-gray-200 dark:bg-gray-700 " />
-	)
+	return json({ categoryTitle, allArticles })
 }
 
 export default function NewsCategoryPage() {
-	const { categoryTitle } = useLoaderData<typeof loader>()
+	const { categoryTitle, allArticles } = useLoaderData<typeof loader>()
 	return (
 		<>
 			<div className="container py-16">
 				<h2 className="text-h2">{categoryTitle}</h2>
-			</div>
-			<div className="grid grid-cols-5 grid-rows-2 gap-6">
-				<WireframeBlock />
-				<WireframeBlock />
-				<WireframeBlock />
-				<WireframeBlock />
-				<WireframeBlock />
-				<WireframeBlock />
-				<WireframeBlock />
-				<WireframeBlock />
-				<WireframeBlock />
-				<WireframeBlock />
+
+				<div className="grid grid-cols-5 grid-rows-2 gap-6">
+					{allArticles.map(article => (
+						<div className=" bg-red-600 p-6" key={article.id}>
+							<h3>{article.title}</h3>
+							<p>{article.category?.name || 'General News'}</p>
+						</div>
+					))}
+				</div>
 			</div>
 		</>
 	)
