3738370d9c120fc36fa6bc5e0b821bccf73c31f3
rwilson1234 <0554244@hc.ac.je>
2025-01-29 20:29:14 +0000

    about us under constustion

--- app/routes/about-us.tsx
+++ app/routes/about-us.tsx
@@ -1,7 +1,24 @@
 export default function AboutUsRoute() {
 	return (
 		<main className="container py-16">
-			<h1 className="text-mega">About us</h1>
+			<div className="flex items-center">
+				<div className="flex-1">
+					<h1 className="text-mega">About us</h1>
+					<h3 className="mt-10 text-4xl">Currently under construction</h3>
+					<p className="mr-40 mt-8 text-xl">
+						We are working hard to bring you a better experience. Please check
+						back soon!
+					</p>
+				</div>
+				<div className="flex-1">
+					<img
+						src="/app/assets/jpg/under-construction2.jpg"
+						alt="under construction"
+						className="md:w-192 h-48 w-96 object-contain md:h-96 lg:h-auto lg:w-full"
+					/>
+				</div>
+			</div>
+			<div className="mx-auto my-8 w-[80%] border-t-2 border-black"></div>
 		</main>
 	)
 }
